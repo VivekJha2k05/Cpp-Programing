dcf354bab1cbb558d5de55c7915bf64daa64b8e0
VivekJha2k05 <vjha00900@gmail.com>
2025-08-29 14:23:32 +0530

    Add files via upload

---
+++ pattern_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int row=1;row<=4;row++)
+    {
+        for( int column=1;column<=4;column++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+} 
